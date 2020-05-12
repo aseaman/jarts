2dfbdb2305accd6046d701f7d89f05dc61673b7f
Aaron Seaman <aaron.seaman@gmail.com>
2020-05-11 21:28:22 -0500

    More project setup

---
+++ src/pages/Game/Game.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function() {
+    return <h3>Games go here</h3>
+}
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function() {
+    return <h3>Home of jarts</h3>
+}
